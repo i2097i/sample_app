e09155dd2933ef9fe95f434e7c8b5b0dcd6a3cdb
i2097i <i2097i@hotmail.com>
2014-08-13 01:08:21 -0400

    last changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def logo
+		image_tag("logo.png", :alt => "Sample App", :class => "round")
+	end
+
+	def title
+		if @title.empty?
+			"empty"
+		else
+			@title
+		end
+	end
 end
