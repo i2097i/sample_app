a29988d56c965db24317682b1323ed1b567791e2
i2097i <i2097i@hotmail.com>
2014-09-01 23:08:44 -0400

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140831015054) do
 
   create_table "microposts", force: true do |t|
